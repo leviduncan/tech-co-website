c0e1c1165a55a1a31f56267b91e317b50e089191
darrin@darrinduncan.com <darrin@darrinduncan.com>
2023-10-13 22:40:51 -0400

    Hero layout update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero">
+        <div className="item1 item"></div>
+        <div className="item2 item"></div>
+        <div className="item3 item"></div>
+        <div className="item4 item"></div>
+    </div>
   )
 }
 
